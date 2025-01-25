33d5ecef2e4f462a13cf48031f30cf8afbe693ab
rajpal <raj.palvm19@gmail.com>
2025-01-25 10:37:12 +0530

    Add responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav className="bg-gray-800 text-white p-4 rounded-md mb-6" >
+    <nav className="bg-gray-800 text-white p-4 rounded-md mb-6">
       <div className="container mx-auto">
-        <h1 className="text-lg font-bold text-center ">Inventory Management</h1>
+        <h1 className="text-lg font-bold text-center md:text-left">Inventory Management</h1>
       </div>
     </nav>
   );
